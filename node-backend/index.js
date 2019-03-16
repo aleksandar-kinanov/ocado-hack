8f227affc6b2a9d24b6e68083ea95f7794f68d5d
Nikolay Nikolov <nikolov1618@gmail.com>
2019-03-16 18:37:11 +0200

    Adding express

--- node-backend/index.js
+++ node-backend/index.js
@@ -1,7 +1,8 @@
 require('@tensorflow/tfjs-node');
+const express = require('express');
+const _ = require('lodash');
 const loadCSV = require('./load-csv');
 const LogisticRegression = require('./regression');
-const _ = require('lodash');
 
 const { features, labels, testFeatures, testLabels } = loadCSV(
   './data_new.csv',
@@ -29,7 +30,25 @@ const regression = new LogisticRegression(features, _.flatMap(labels), {
 regression.train();
 
 const accuracy = regression.test(testFeatures, _.flatMap(testLabels));
-console.log('Accuracy: ', accuracy);
+console.log('Accuracy is: ', accuracy);
 
-const prediction = regression.predict([[2, 25, 430]]).get([0]) + 1;
-console.log('Prediction: ', prediction);
+const app = express();
+const port = 3030;
+
+app.get('/prediction', (req, res) => {
+  const { product, day, humidity, voltage } = req.query;
+
+  if (!product || !day || !humidity || !voltage) {
+    return res.send('Missing params!');
+  }
+
+  if (product === 'orange') {
+    const prediction = regression.predict([[day, humidity, voltage]]).get([0]) + 1;
+
+    return res.send(`Predicted ${product} quality is: ${prediction}!`);
+  }
+
+  return res.send('Invalid product!');  
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
