724693c1228a50dfd73bb400714f124b257ae60c
Nikolay Nikolov <nikolov1618@gmail.com>
2019-03-16 14:34:49 +0200

    Moving to multinominal

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 require('@tensorflow/tfjs-node');
 const loadCSV = require('./load-csv');
 const LogisticRegression = require('./regression');
+const _ = require('lodash');
 
 const { features, labels, testFeatures, testLabels } = loadCSV(
-  './houses.csv',
+  './data.csv',
   {
     dataColumns: ['bedrooms', 'sqft_living', 'condition', 'grade'],
     labelColumns: ['price'],
     shuffle: true,
-    splitTest: 10000,
+    splitTest: 7000,
     converters: {
       price: value => {
-        return value > 500000 ? 1 : 0;
+        if (value < 700000) return [1,0,0];
+        if (value < 1200000) return [0,1,0];
+        return [0,0,1];
       }
     }
   }
 );
 
-const regression = new LogisticRegression(features, labels, {
-  learningRate: 0.5,
+const regression = new LogisticRegression(features, _.flatMap(labels), {
+  learningRate: 1,
   iterations: 100,
-  batchSize: 1000
+  batchSize: 500
 });
 
 regression.train();
 
-console.log('Testing accuracy is:', regression.test(testFeatures, testLabels));
+// const accuracy = regression.test(testFeatures, _.flatMap(testLabels));
+// console.log('Accuracy: ', accuracy);
+
+// regression.predict([
+//   [4, 2950, 3, 9]
+// ]).print();
--- regression.js
+++ regression.js
@@ -8,15 +8,15 @@ class LogisticRegression {
     this.costHistory = [];
 
     this.options = Object.assign(
-      { learningRate: 0.1, iterations: 1000, decisionBoundary: 0.5 },
+      { learningRate: 0.1, iterations: 1000 },
       options
     );
 
-    this.weights = tf.zeros([this.features.shape[1], 1]);
+    this.weights = tf.zeros([this.features.shape[1], this.labels.shape[1]]);
   }
 
   gradientDescent(features, labels) {
-    const currentGuesses = features.matMul(this.weights).sigmoid();
+    const currentGuesses = features.matMul(this.weights).softmax();
     const differences = currentGuesses.sub(labels);
 
     const slopes = features
@@ -54,18 +54,16 @@ class LogisticRegression {
   predict(observations) {
     return this.processFeatures(observations)
       .matMul(this.weights)
-      .sigmoid()
-      .greater(this.options.decisionBoundary)
-      .cast('float32');
+      .softmax()
+      .argMax(1);
   }
 
   test(testFeatures, testLabels) {
     const predictions = this.predict(testFeatures);
-    testLabels = tf.tensor(testLabels);
+    testLabels = tf.tensor(testLabels).argMax(1);
 
     const incorrect = predictions
-      .sub(testLabels)
-      .abs()
+      .notEqual(testLabels)
       .sum()
       .get();
 
@@ -89,16 +87,21 @@ class LogisticRegression {
   standardize(features) {
     const { mean, variance } = tf.moments(features, 0);
 
-    this.mean = mean;
-    this.variance = variance;
+    const filler = variance
+      .cast('bool')
+      .logicalNot()
+      .cast('float32');
 
-    return features.sub(mean).div(variance.pow(0.5));
+    this.mean = mean;
+    this.variance = variance.add(filler);
+
+    return features.sub(mean).div(this.variance.pow(0.5));
   }
 
   recordCost() {
-    const guesses = this.features.matMul(this.weights).sigmoid();
+    const guesses = this.features.matMul(this.weights).softmax();
 
-    const termOne = this.labels.transpose().matMul(guesses.log());
+    const termOne = this.labels.transpose().matMul(guesses.add(1e-7).log());
 
     const termTwo = this.labels
       .mul(-1)
@@ -108,6 +111,7 @@ class LogisticRegression {
         guesses
           .mul(-1)
           .add(1)
+          .add(1e-7)
           .log()
       );
 
