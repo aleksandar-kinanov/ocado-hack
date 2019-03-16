07e636ea234e3b1199466d8453ea8d2faaf4f96f
Nikolay Nikolov <nikolov1618@gmail.com>
2019-03-16 21:25:05 +0200

    Wiring up the db reading

--- react-frontend/src/Card.js
+++ react-frontend/src/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { default as CardMaterial } from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -18,10 +18,28 @@ const styles = {
 };
 
 const Card = ({ classes, text, image }) => {
+  const [caption, setCaption] = useState(`Click to see how your ${text} are doing...`);
+  
   return (
-    <CardMaterial className={classes.card} onClick={() => {
-      axios.get('http://localhost:3030/predict?product=apple&day=6&humidity=53&voltage=660')
-        .then(data => console.log(data));
+    <CardMaterial className={classes.card} onClick={async () => {
+      const { data: dataSamples } = await axios.get('http://172.26.17.133:5000/data');
+
+      const dataLength = dataSamples.data.length;
+
+      const day = (dataSamples.data.reduce((prev, current) => prev + current[2], 0)) / dataLength;
+      const humidity = (dataSamples.data.reduce((prev, current) => prev + current[0], 0)) / dataLength;
+      const voltage = (dataSamples.data.reduce((prev, current) => prev + current[1], 0)) / dataLength;
+
+      const { data: newCaptionData } = await axios.get(
+        `http://localhost:3030/predict?product=${text.slice(0, -1)}&day=${day}&humidity=${humidity}&voltage=${voltage}`
+      );
+
+      const caption = newCaptionData.message
+        .replace('1', '(1)First Grade')
+        .replace('2', '(2)Second Grade')
+        .replace('3', '(3)Third Grade');
+
+      setCaption(caption);
     }}>
       <CardActionArea>
         <CardMedia
@@ -33,8 +51,8 @@ const Card = ({ classes, text, image }) => {
           <Typography gutterBottom variant="h5" component="h2">
             {text.charAt(0).toUpperCase() + text.slice(1)}
           </Typography>
-          <Typography component="p">
-            See how your {text} are doing
+          <Typography component="p" variant="h6">
+            {caption}
           </Typography>
         </CardContent>
       </CardActionArea>
