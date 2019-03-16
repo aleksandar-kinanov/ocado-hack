07c030fc0d2a3a39bf7d82d1673e4148803ac99f
Tsvetan Ivanov <tsvetan.ivanov@accedia.com>
2019-03-17 00:17:05 +0200

    IoT/add changing modes with buttons

--- IoT/les-waste.ino/les-waste.ino.ino
+++ IoT/les-waste.ino/les-waste.ino.ino
@@ -2,6 +2,11 @@
 
 dht DHT;
 
+// Button pins and states
+const int changeModeButtonPin = 2;
+const int changeValButtonPin = 3;
+int prevButtonState = LOW;
+
 int analogInputVoltmeter = A1;
 int dhtInputPin = A0;
 float Vout = 0.00;
@@ -9,15 +14,59 @@ float Vin = 0.00;
 float R1 = 10000.00; // resistance of R1 (10K)
 float R2 = 1000.00; // resistance of R2 (1K)
 
+
+const short READ_PRODUCT_TYPE = 0;
+const short READ_DECAY_DAYS = 1;
+const short FEED_DATA = 2;
+// 0 - Read Product Type
+// 1 - Read Decay Days
+// 2 - Feed data
+short mode = READ_PRODUCT_TYPE;
+
 void setup() {
   Serial.begin(9600);
   
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(analogInputVoltmeter, INPUT);
   pinMode(dhtInputPin, INPUT);
+
+  // Setup button pins
+  pinMode(changeModeButtonPin, INPUT);
+  pinMode(changeValButtonPin, INPUT);
 }
 
 void loop() {
+  Serial.print("Mode = ");
+  Serial.println(mode);
+  if (mode == FEED_DATA) {
+    feedData();
+  } else if (mode == READ_PRODUCT_TYPE) {
+    readProductType();
+  } else if (mode == READ_DECAY_DAYS) {
+    readDecayDays();
+  }
+  
+  checkMode();
+}
+
+void checkMode() {
+  int changeModeButtonState = digitalRead(changeModeButtonPin);
+  if (changeModeButtonState == HIGH && prevButtonState == LOW) {
+    mode++;
+  }
+  prevButtonState = changeModeButtonState;
+}
+
+void readDecayDays() {
+  
+}
+
+void readProductType() {
+  //Print Choose product on LCD
+  
+}
+
+void feedData() {
   float Vin = getVolts();
   DHT.read11(dhtInputPin);
   
@@ -29,6 +78,7 @@ void loop() {
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
+
   delay(1000);
 }
 
